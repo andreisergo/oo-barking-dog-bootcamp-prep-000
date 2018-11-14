2b1d27f465938b3bb83af8815ccbc1184e9d9a90
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-14 04:17:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 # Your code goes here!
 
-class
+class Dog 
+  def initialize(name, bark)
+    @name = name
+    @bark = bark
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+
+end
